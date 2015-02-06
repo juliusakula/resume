ef262969885da93b39a488712a4ec56b133f4023
Justin Murdock <jmurdock@lyris.com>
2015-02-06 04:33:00 -0800

    new profile pic

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,12 +5,13 @@ var bio = {
         twitter: "https://twitter.com/JuicetinMurdock",
         location: "Berkeley, CA, USA",
         mobile: "1-555-123-5555",
-        email: "jmurdock-resume@im.uptilt.com",
-        github: "https://github.com/juliusakula"
+        email: "jmurdock-idontcheckthisemail@im.uptilt.com",
+        github: "https://github.com/juliusakula",
+        linkedin: "https://www.linkedin.com/profile/view?id=322687836"
     },
     welcomeMessage: "Welcome to my Interactive Resume!",
     skills: ["Javascript", "Grunt", "PHP / HTML / CSS", "MySQL", "Node.js", "Angular.js"],
-    biopic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw01USki2nz8a3B2te3R8VgKuPUNarqlV2c3w9GwrLBw7WDyGTZA',
+    biopic: 'images/profile_pic.jpg',
     display: function(){
         var formattedName = HTMLheaderName.replace("%data%", this.name),
             formattedContactMe = "",
@@ -47,9 +48,9 @@ var work = {
             title: 'Junior Web Developer',
             location: 'Emeryville, CA, USA',
             dates: 'October 2013 - Present',
-            description: '<ul><li>While on the <i>Quality Assurance Team</i> I wrote testcases for our product and held the record for most test cases written in one day, myself only knowing programming for less than 4 months, competing against the team in India for that record. My mentor during this time helped me a great deal with seeing things from a higher level, and the importance of communication. </li>'+
-            '<li>While on the <i>Product Engineering Team</i> I wrote a PHP Data Access Object library that read from a configuration file in order to query a database or series of similar databases and return the results as one set. By creating this seperation of concerns between data access and data stores, scripts could be stored in any version control system without including passwords by simply omitting the password file. It was a front-end agnostic library, so the data could be viewed in any way. It was used to write better billing scripts, among other things.</li>'+
-            '<li>While on the <i>Deliverability Team</i>, I learned practical Git, HTML & CSS, Javascript, Angular.JS, Node.JS, Grunt, and User Interface Design. My first task was to refine ETL (Extract, Transform, Load) scripts already in the Deliverability Team\'s repertiour by using my Data Access Object PHP libraries. I combined the refined tools into an API and also wrote a robust fluid front end tool set for the Deliverability Team to use, featuring <a class="inline-plz" href="http://ui-grid.info/">angular-ui/ui-grid</a> and AJAX.</li></ul>' 
+            description: '<ul><li>While on the <i>Quality Assurance Team</i> I wrote testcases for our product and held the record for most test cases written in one day, myself only knowing programming for less than 4 months at the time, competing against the team in India for that record. My mentor during this time helped me a great deal with seeing things from a higher level, and the importance of communication. During this time I was programming in Java.</li>'+
+            '<li>While on the <i>Product Engineering Team</i> I wrote an Object Oriented PHP Data Access Object library that read from a configuration file in order to query a database or series of similar databases and return the results as one set. By configuring multiple databases into the same object it required very little code to extract data from a multitude of locations. By creating this seperation of concerns between data access and data storages, scripts could be stored in version control systems without including passwords by simply omitting the password file. It was a front-end agnostic library, so the data could be viewed in any way. It was used to write better billing scripts for the company, among other things.</li>'+
+            '<li>Then, I recieved a promotion from <i>Intern</i> to <i>Junior Software Engineer</i> and I joined the <i>Deliverability Team</i>. I learned practical Git, HTML & CSS, Javascript, Angular.JS, Node.JS, Grunt, and User Interface Design. My first task was to refine ETL (Extract, Transform, Load) scripts already in the Deliverability Team\'s repertiour by using my Data Access Object PHP libraries. I combined the refined tools into a custom API, and also wrote a robust front end tool set for the Deliverability Team to use, featuring <a class="inline-plz" href="http://ui-grid.info/">angular-ui/ui-grid</a>, beautiful <a class="inline-plz" href="http://codepen.io/paulrose/pen/mEGLg">css forms</a>, grunt scaffolding for rapid creation of new tools, and lots of AJAX.</li></ul>' 
         },
         {
             employer: 'Ponderosa Farms, Inc',
@@ -81,23 +82,17 @@ var work = {
 var projects = {
     projects: [
         {
-            title: 'Building Computers',
-            dates: '2003 - Present',
-            description: '<ul><li>I\'ve built dozens of computers since I was very young -- media centers, gaming computers, bitcoin mining computers, and even a watercooled computer. I know quite a bit about computer hardware. </li></ul>',
-            images: []
-            
-        },
-        {
             title: 'Bitcoin Mining',
             dates: 'November 2013 - February 2014',
-            description: '<ul><li>During the Bitcoin bubbled of 2013 where BTC was valued over $1000, I mined more than 3.5 BTC with a computer I built for mining. The mined BTC paid for the multi-GPU mining computer twice over. I also learned a lot about cryptography in the process, and helped a few friends mine on their computers and on the cloud. In addition to mining at home, I mined on cloud mining websites and on Amazon Web Service VMs.</li></ul>',
-            images: ['images/small_hung_up.jpg','images/small_on_table.jpg']
+            description: '<ul><li>During the Bitcoin bubbled of 2013 where BTC was valued over $1000, I built a computer to mine BTC. The mined BTC paid for the multi-GPU mining computer twice over. I also learned a lot about cryptography in the process, and helped a few friends mine on their computers and on the cloud. In addition to mining at home I mined BTC on cloud mining services, and on Amazon Web Service EC2 VMs.</li>'+
+            '<li>I\'ve built dozens of computers since I was very young -- media centers, gaming computers, bitcoin mining computers, even one watercooled computer. </li></ul>',
+            images: ['images/small_hung_up.jpg','images/small_on_table.jpg','images/small_parachord.jpg']
             
         },
         {
             title: 'Free Time Coding',
             dates: 'September 2013 - Future',
-            description: '<ul><li>While employed at Lyris, I was also learning to code in my free time. I have used <a class="inline-plz" href="https://www.coursera.org/">Coursera</a>, <a class="inline-plz" href="http://www.codecademy.com/">Code Academy</a> and <a class="inline-plz" href="https://www.codeschool.com/">Code School</a> to learn a wide variety of topics from Javascript to Ruby on Rails to Git. I completed a verified certificate for Ruby on Rails from the University of New Mexico with distinction. I completed the entire PHP path on Code Academy, and several entire <a class="inline-plz" href="https://www.codeschool.com/paths">paths on Code School</a> including <a class="inline-plz" href="https://www.codeschool.com/paths/html-css">HTML & CSS</a>, <a class="inline-plz" href="https://www.codeschool.com/paths/javascript">Javascript</a>, <a class="inline-plz" href="https://www.codeschool.com/paths/git">Git</a>, and <a class="inline-plz" href="https://www.codeschool.com/paths/electives">Electives</a>. I\'ve mastered four paths on Code School, with 29 courses and over 1400 code challenges completed. Links to certificates and report cards are under Education.</li></ul>',
+            description: '<ul><li>While employed at Lyris, I was also learning to code in my free time. I have used <a class="inline-plz" href="https://www.coursera.org/">Coursera</a>, <a class="inline-plz" href="http://www.codecademy.com/">Code Academy</a> and <a class="inline-plz" href="https://www.codeschool.com/">Code School</a> to learn a wide variety of topics from Javascript to Ruby on Rails to Git. I took a coursera course on Ruby on Rails from the University of New Mexico and earned a <a class="inline-plz" href="https://www.coursera.org/account/accomplishments/certificate/7C3P4MSZZJ">verified certificate</a> with distinction. I completed the entire <a class="inline-plz" href="http://www.codecademy.com/en/tracks/php">PHP path</a> on Code Academy, and several entire <a class="inline-plz" href="https://www.codeschool.com/paths">paths on Code School</a> including <a class="inline-plz" href="https://www.codeschool.com/paths/html-css">HTML & CSS</a>, <a class="inline-plz" href="https://www.codeschool.com/paths/javascript">Javascript</a>, <a class="inline-plz" href="https://www.codeschool.com/paths/git">Git</a>, and <a class="inline-plz" href="https://www.codeschool.com/paths/electives">Electives</a>. I\'ve mastered four paths on Code School, with 29 courses and over  <a class="inline-plz" href="https://www.codeschool.com/users/juliusakula">1400 code challenges completed</a>.</li></ul>',
             images: ['https://d1tijy5l7mg5kk.cloudfront.net/assets/paths/badge-html-css-2bcd13b6f0e8092b6e4136a448ea3244.svg','https://d1tijy5l7mg5kk.cloudfront.net/assets/paths/badge-javascript-6d3dadcfccd99554cff7af219749573e.svg', 'https://d1tijy5l7mg5kk.cloudfront.net/assets/paths/badge-git-e1fad31eec7e3bd5ede23600b3e29808.svg', 'https://d1tijy5l7mg5kk.cloudfront.net/assets/paths/badge-electives-027e9b36116e380db027b189555dfb0d.svg']
             
         }
@@ -137,7 +132,7 @@ var education = {
         name: 'Berkeley City College',
         location: 'Berkeley, CA, USA',
         degree: 'General Education',
-        majors: ['Transfer Requirements'],
+        majors: ['none'],
         dates: 'August 2013 - December 2013',
         url: 'http://web.peralta.edu/blog/category/berkeley-city-college/'
     }
